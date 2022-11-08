b06e2546f4079f0a57ac39604e6ce380e08186ce
GissellGeorge <gissellgeorge@gmail.com>
2022-11-08 08:09:23 -0500

    mi perfil

--- index.php
+++ index.php
@@ -17,16 +17,16 @@ include("marco-right.php");
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
                 Posteado por:  Elaine George<br>
-                Fecha: 2022-10-30 <br>
+                Fecha: 2022-11-08 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Stalin Francis, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo">Hola, yo soy Elaine George, Tenológa en manteniento de hadware y software, actualmente me encuentro estudiando en la universidad técnica Luis Vargas Torres la carrrea de Tic's (Tecnología de la imformación y comunicación) con el objetivo deobtener mi titulo como ingeniera en la carrera antes mencionada paraaportar de una manera mas profesional a la sociedad</p>
+                   <p class="eys-parrafo">Cuando inicie mis estudios en la secundaria ya estando en el bachillerato opte por estudiar Química y Biología,pero sentía la necesidad de aprender computación, fue desde entonces que cuando terminé el bachillerato empece a estudiar la carrera de informáticase me hizo un poco dificil pero con el tiempo la fui entendiendo a tal punto que me desenvuelvo muy bien con la especialidadque e llegado a amar la carrera. </p>
+<p class="eys-parrafo"> El primer lenguaje de programación que aprendi ya en mis estudos suoerior fue con el leguaje de c#, se me hizo complicado el lenguaje pero gracias a Dios puse todo mi esfuerzo en aprender y lo logré aprobrando mi primer nivel en programanción.	</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,23 +34,23 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fiscal mixto 5 de agosto donde me gradue en el año 2006, de la especialidad de Químico-Biologo ,apenas me gradue  empece a estudiar en el instituto técnico sperior Eloy Alfaro, la carrera duró 3 años y me gradue en el año 2016.Despues me dedique a trabajar pero no ejerci la profesión, laboraba como asesora en belleza en la ciudad de guayaquil.Decidi regresarme a Esmeraldas y decidi estudiar para sacar mi imgenieria en Sistemas donde ya curso el 7mo ciclo de la carrera ya muy pronto a obtner mi segundi título profesional.</p>
+                   <p class="eys-parrafo"></p>
+<p class="eys-parrafo"></p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
-    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil tecnológico</h2> 
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">Mi perfil tecnológico manejo varios sistemas o aplicaiones como el paquete de office, manejo varias páginas web del estado para realizar tramites, realizo matenimientos preventivos y correctivos a las pc, instalo y desintalo programas, tengo conocimientos de varios lenguajes de programación como c#,c++, java, y bases de datos Sql, para lo que es el frond end e trabajado en css, bootras</p> 
+                   <p  class="eys-parrafo">Hoy en día solo me dedico a estudiar al 100% por que quiero aprender mi profesión muy bien para ser una profesional de excelencia.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Soy amante a la belleza y lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo todo tipo de informafión que se útil en mi diario vivir para cada día ser una mejor persona,ademas me estoy preparando porque cuando termine mi carrera implementaré mi propia empresa en beneficio a la naturaleza.</p>
        
 
                    
